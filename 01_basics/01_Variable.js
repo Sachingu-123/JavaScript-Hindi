9a0cff76f921b6ad0ad795a74577b9ec7a3f278a
Sachin Gupta <121962671+Sachingu-123@users.noreply.github.com>
2023-08-11 14:06:26 +0000

    content config done

---
+++ 01_basics/01_Variable.js
@@ -0,0 +1,11 @@
+const accountId=12345
+let accountEmail="sachingu@gmail.com"
+var accountPass="212121"
+accountCity="bangalore"
+// accountId=54321
+// console.log(accountId); 
+console.table([accountEmail,accountPass,accountCity])
+accountPass="321"
+console.log(accountPass);
+accountCity="bihar"
+console.log(accountCity);
