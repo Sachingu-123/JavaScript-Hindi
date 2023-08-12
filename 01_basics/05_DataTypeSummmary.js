76d5b7d69ca1e85b7175a5f64be36c05cb4aae20
Sachin Gupta <121962671+Sachingu-123@users.noreply.github.com>
2023-08-12 18:27:57 +0000

    summary of DT

---
+++ 01_basics/05_DataTypeSummmary.js
@@ -0,0 +1,10 @@
+const score=100.54444
+console.log(typeof(score))
+const score1=100
+console.log(typeof(score1))
+const str="hitesh"
+console.log(typeof(str))
+const str1=null
+console.log(typeof(str1))
+let temp=undefined
+console.log(typeof(temp))
