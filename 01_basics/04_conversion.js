b2467faba7250644be034c0ce209e42df3a174d5
Sachin Gupta <121962671+Sachingu-123@users.noreply.github.com>
2023-08-12 10:39:07 +0000

    conversion done

---
+++ 01_basics/04_conversion.js
@@ -0,0 +1,35 @@
+// data type should must be same while comaparision
+// console.log(2>1)
+// console.log(2==2)
+// console.log(2<1)
+// console.log(2>=2)
+// console.log(2<=1)
+
+// console.log(true>2)
+// console.log(true>-1)
+// console.log(true==1)
+// console.log(false==0)
+// console.log(false>-1)
+// console.log(false>1)
+
+// console.log("2">1)
+// console.log("2"==2)
+
+// console.log(null>0)
+// console.log(null==0)
+// console.log(null>=0)
+// console.log(null<0)
+// console.log(null<=0)
+
+// console.log(undefined>0)
+// console.log(undefined==0)
+// console.log(undefined>=0)
+// console.log(undefine==undefined)
+
+
+// === strict check
+console.log("2"===2)
+console.log(2==="2");
+
+console.log("sachin">"gupta")
+console.log("gupta">"sachin");
