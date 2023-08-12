f6af2623620563f65557d5e86d9969426e460a32
Sachin Gupta <121962671+Sachingu-123@users.noreply.github.com>
2023-08-12 10:17:16 +0000

    operation

---
+++ 01_basics/03_operationOnDT.js
@@ -0,0 +1,42 @@
+/*
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2/2)
+console.log(4/2)
+console.log(4%2)
+console.log(2%2)
+console.log(0/2)
+console.log(2/0)
+console.log(0%2)
+console.log(2%0) */
+
+let str2="gupta"
+let str1="sachin"
+let str3=str1+str2
+// console.log(str3)
+
+/*
+console.log(2+2)
+console.log(2+"3")
+console.log("3+2")
+console.log(2+2+"3")
+console.log("3"+2+2)
+console.log(2-"2"+2) 
+*/
+
+// pre and postfix 
+
+ let getScore=100
+ getScore++
+// console.log(getScore)
+
+let w=7
+let z=w++
+console.log(w)
+console.log(z)
+
+let x=8
+let y=++x
+console.log(y)
+console.log(x)
