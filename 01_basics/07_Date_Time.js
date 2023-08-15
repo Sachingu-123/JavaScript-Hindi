11dc9fed768728887d6c9195473c89d35bf484f2
Sachin Gupta <121962671+Sachingu-123@users.noreply.github.com>
2023-08-15 12:48:26 +0000

    date and time done

---
+++ 01_basics/07_Date_Time.js
@@ -0,0 +1,29 @@
+// console.log(Date)
+let myDate=new Date()
+// console.log(myDate)
+// console.log(myDate.toDateString())
+// console.log(myDate.toLocaleDateString())
+// console.log(myDate.toLocaleString())
+// console.log(typeof(myDate))
+
+// let myCreatedDate=new Date(2023,0,15)
+// let myCreatedDate=new Date(2023,0,15,5,3)
+// let myCreatedDate=new Date("08-15-2023")
+let myCreatedDate=new Date("2023-08-15")
+// console.log(myCreatedDate)
+// console.log(myCreatedDate.toDateString());
+// console.log(myCreatedDate.getTime());
+
+
+// let myTimeStamp= Date.now()
+// console.log(myTimeStamp)
+// console.log(Date.now()/1000)
+
+
+let newDate= new Date()
+console.log(newDate)
+console.log(newDate.getDate())
+console.log(newDate.getMonth())
+console.log(newDate.getFullYear())
+
+
