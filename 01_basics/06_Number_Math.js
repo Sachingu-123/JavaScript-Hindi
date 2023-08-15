dff6f985ce7745746c17c9e620fd24302fd44117
Sachin Gupta <121962671+Sachingu-123@users.noreply.github.com>
2023-08-15 11:01:52 +0000

    Number

---
+++ 01_basics/06_Number_Math.js
@@ -0,0 +1,35 @@
+const score=400
+// console.log(score)
+
+
+const balance=new Number(400)
+// console.log(balance)
+// console.log(balance.toString())
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherValue=123.998
+// console.log(typeof(otherValue))
+// console.log(otherValue.toFixed(2))
+// console.log(otherValue.toPrecision(2));
+
+const value=1000000
+// console.log(value.toLocaleString())   -->us standard
+// console.log(value.toLocaleString("en-IN"))
+
+
+// ***************** Math Library ***************
+
+// console.log(Math)
+// console.log(Math.abs(-4))
+// console.log(Math.round(4.4))
+// console.log(Math.ceil(4.2))
+// console.log(Math.floor(4.9))
+
+
+// console.log(Math.max(1,2,3,4,5))
+// console.log(Math.min(1,2,3,4))
+
+console.log(Math.random()*10)
+console.log(Math.random()*10+1)
+console.log((Math.random()*10)+1);
